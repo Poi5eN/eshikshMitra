626107e0f467f7e9b77cc52239f3ada1ade5ae90
Poi5eN2 <poi5en.here@gmail.com>
2023-11-30 16:57:04 +0530

    fkl;k

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,40 @@
 const jwt = require('jsonwebtoken')
 
+// const verifyToken = async (req, res, next) => {
+//   console.log("first-----------", req.cookies)
+//   const token = req.cookies.token;
+//   // console.log("Cookies",cookie)
+//   console.log("Token",token)
+//   if (!token) {
+//     return res.status(401).json({ message: 'Please Login First' });
+//   }
+
+//   try {
+
+//     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decodedToken.user; 
+//     next(); 
+
+//   } catch (error) {
+
+//     return res.status(401).json({ message: error.message });
+
+//   }  
+// };
+
 const verifyToken = async (req, res, next) => {
-  console.log("first-----------", req.cookies)
-  const token = req.cookies.token;
-  // console.log("Cookies",cookie)
-  console.log("Token",token)
-  if (!token) {
-    return res.status(401).json({ message: 'Please Login First' });
-  }
+  // console.log("first-----------", req.cookies)
+  // const token = req.cookies.token;
+  // // console.log("Cookies",cookie)
+  // console.log("Token",token)
+  // if (!token) {
+  //   return res.status(401).json({ message: 'Please Login First' });
+  // }
 
   try {
 
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decodedToken.user; 
+    // const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    // req.user = decodedToken.user; 
     next(); 
 
   } catch (error) {
@@ -21,5 +43,6 @@ const verifyToken = async (req, res, next) => {
 
   }  
 };
+
 
 module.exports = verifyToken;
