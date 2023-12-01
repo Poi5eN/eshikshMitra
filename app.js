e14de5d692882f255f8a508ffb5952770164190d
Poi5eN2 <poi5en.here@gmail.com>
2023-12-01 16:28:01 +0530

    ijuklj

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const routes = require('./routes/route')
 const cors = require("cors");
 const app = express()
 
+
 app.use(express.json())
 const corsOptions = {
     origin: 'http://localhost:3000',
