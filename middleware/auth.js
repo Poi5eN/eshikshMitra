fd9a355a58cd92452bfd2d5d7b493b62a2d26654
Ajay Raj <ajayraj072001@gmail.com>
2023-12-01 10:52:19 +0530

    up

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = async (req, res, next) => {
-  console.log("first-----------", req.cookies)
-  const token = req.cookies.token;
-  // console.log("Cookies",cookie)
-  console.log("Token",token)
-  if (!token) {
-    return res.status(401).json({ message: 'Please Login First' });
+  console.log("auhto-----",req.headers.authorization)
+  console.log("Aitho-----",req.headers.Authorization)
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Invalid or missing Authorization header' });
   }
+
+  const token = authorizationHeader.split(' ')[1];
 
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decodedToken.user; 
     next(); 
   } catch (error) {
-
     return res.status(401).json({ message: error.message });
-
   }  
 };
 
