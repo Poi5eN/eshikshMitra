7d1cb053940404f63ff90881e53426fe56b54115
Poi5eN2 <poi5en.here@gmail.com>
2023-12-02 12:17:55 +0530

    jkhk

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 const corsOptions = {
-    origin: 'https://eshikshamitra.in',
+    origin: 'http://localhost:3000',
     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Include cookies and authentication headers
   };
