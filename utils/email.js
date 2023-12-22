a20618ee62aa153f1c786714646a26ce33ae661b
Parveen-Pal <parveenpal02001@gmial.com>
2023-12-22 12:24:35 +0530

    resolve error of sent email

--- utils/email.js
+++ utils/email.js
@@ -1,19 +1,23 @@
 const nodemailer = require('nodemailer')
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    // auth: {
-    //   user: 'karan07200119@gmail.com',
-    //   pass: 'wzzthlkezgnfniif'
-    // },
-    auth: {
-      user: 'parveenpal02001@gmail.com',
-      pass: 'wrvr dukx fmkf gzdp'
-    },
-  });
+
   
   // Create a function to send an email
   const sendEmail = (toEmail, subject, htmlContent) => {
+
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      // auth: {
+      //   user: 'karan07200119@gmail.com',
+      //   pass: 'wzzthlkezgnfniif'
+      // },
+      auth: {
+        user: 'parveenpal02001@gmail.com',
+        pass: 'wrvr dukx fmkf gzdp'
+      },
+    });
+
     const mailOptions = {
       from: 'parveenpal02001@gmail.com',
       to: toEmail,
