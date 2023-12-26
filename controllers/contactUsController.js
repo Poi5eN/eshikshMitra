8231754ac85bdac52947ef7ecd89cdd1e0332187
Parveen-Pal <parveenpal02001@gmial.com>
2023-12-26 15:16:32 +0530

    contact us api

--- controllers/contactUsController.js
+++ controllers/contactUsController.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const ContactUs = require('../models/ContactUs'); 
-const nodemailer = require('nodemailer');
+const sendEmail = require('../utils/email');
+// const nodemailer = require('nodemailer');
 
 
 exports.contactUs = async (req, res) => {
@@ -35,6 +36,26 @@ exports.contactUs = async (req, res) => {
         // };
     
         // await transporter.sendMail(mailOptions);
+
+
+        subject: `New Inquiry from [Client Name ${name}]`,
+
+        content = `
+        <html>
+            <body style="background-color: #f4f4f4; padding: 20px; margin: 0;">
+                <h2 style="color: #333;">New Inquiry: Contact Information</h2>
+                <p style="margin-bottom: 10px;"><strong style="color: #555;">Name:</strong> ${name}</p>
+                <p style="margin-bottom: 10px;"><strong style="color: #555;">Email:</strong> ${email}</p>
+                <p style="margin-bottom: 10px;"><strong style="color: #555;">Contact:</strong> ${contact}</p>
+                <p style="margin-bottom: 10px;"><strong style="color: #555;">School Name:</strong> ${schoolName}</p>
+                <p style="margin-bottom: 10px;"><strong style="color: #555;">Message:</strong> ${message}</p>
+            </body>
+        </html>        
+        `,
+
+
+
+        await sendEmail("parveenpal02001@gmail.com", subject, content);
     
         res.status(201).json({ message: 'ContactUs record created and email sent successfully' });
       } catch (error) {
