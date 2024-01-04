5b40ade68687c57f86d6f9c5ebfabdede50fd063
Parveen-Pal <parveenpal02001@gmial.com>
2024-01-04 11:22:36 +0530

    change in server

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 
 const corsOptions = {
   origin:['https://eshikshamitra.in', 'http://localhost:3000'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  methods: '*',
 
   // origin: 'http://localhost:3000',
   credentials: true, // Include cookies and authentication headers
